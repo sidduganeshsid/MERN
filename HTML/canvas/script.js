cbc6be1daf62ecc2579a52ce3997968995b8e2d7
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-06-17 21:28:40 +0530

    add

---
+++ HTML/canvas/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+
+// Add a rectangle at (10, 10) with size 100x100 pixels
+ctx.fillRect(10, 10, 100, 100);
